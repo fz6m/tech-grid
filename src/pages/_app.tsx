53145c29d3f7823f5c4cfd534583383c712275db
fz6m <59400654+fz6m@users.noreply.github.com>
2023-02-12 06:26:45 +0800

    wip: init sc, antd, resetcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+
+// reset style
+import 'normalize.css/normalize.css'
+
+// global style
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,66 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const scSheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      // SSR Render
+      const cache = createCache()
+
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => {
+            const elm = (
+              <StyleProvider cache={cache}>
+                <App {...props} />
+              </StyleProvider>
+            )
+            return scSheet.collectStyles(elm)
+          },
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+
+      // Grab style from cache
+      const styleText = extractStyle(cache)
+
+      scSheet.getStyleElement()
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `</script>${styleText}<script>`,
+              }}
+            />
+            {scSheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      scSheet.seal()
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
