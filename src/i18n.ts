f1e84592c83218463702babf7b491be3005f9fea
fz6m <59400654+fz6m@users.noreply.github.com>
2023-02-12 12:13:43 +0800

    fix: i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,8 @@
-import i18nConfig from '../next-i18next.config'
+export const i18n = {
+  defaultLocale: 'zh',
+  locales: ['zh', 'en'],
+}
 
-export const i18n = i18nConfig.i18n
 export enum EI18nNs {
   common = 'common',
 }
